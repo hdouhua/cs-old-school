e722a1039c5e7d0aaa25bd960d4807662ebbcfe5
hdouhua <yilin.dao@gmail.com>
2022-10-07 20:56:23 +0800

    chore: re-organize

--- kuber/src/kubeadm/reset.sh
+++ kuber/src/kubeadm/reset.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env bash
 
+# drain the node first
+kubectl drain <node name> --delete-emptydir-data --force --ignore-daemonsets
+
 # 重置 kubeadm 安装
 kubeadm reset
 
 # 清除 iptables 规则
 iptables -F && iptables -t nat -F && iptables -t mangle -F && iptables -X
+
+# reset IPVS table
+# ipvsadm -C
 
 # 删除节点
 kubectl delete node <node name>
